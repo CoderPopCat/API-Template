fc47a374a4ad43d37ad753db39fea328c54528f7
CoderPopCat <79563127+CoderPopCat@users.noreply.github.com>
2021-09-10 12:44:33 +0400

    Create endpoint.js

---
+++ routes/endpoint.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "endpoint",
+    run: async(req, res) => {
+        const query = req.query.q;
+        if(!query) return res.json({ error: "Provide something or Pop Cat will eat you!" })
+        res.json({ jsonproperty: `Your query is: ${query}` })
+    }
