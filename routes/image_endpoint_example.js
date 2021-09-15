d70c9dfdfcc32bc3e7e8363b56cd957f1dc93337
CoderPopCat <79563127+CoderPopCat@users.noreply.github.com>
2021-09-15 15:22:04 +0400

    Create image_endpoint_example.js

---
+++ routes/image_endpoint_example.js
@@ -0,0 +1,26 @@
+// Usage: https://yourdomainidk.com/communism?image=https://cdn.popcat.xyz/avatar.png
+
+const Canvas = require("canvas")
+
+module.exports = {
+	name: "communism",
+
+	run: async(req, res) => {
+	const { image } = req.query;
+	if (!image) return res.json({ error: "Please provide an image!" })
+	try {
+		const overlayAvatarURL = "https://miro.medium.com/max/1024/1*L_uBUcOn1-NvGPQFZ4XlrQ.png"
+		const baseAvatar = await Canvas.loadImage(image);
+		const overlayAvatar = await Canvas.loadImage(overlayAvatarURL);
+		const canvas = Canvas.createCanvas(1024, 1024);
+		const ctx = canvas.getContext('2d');
+		ctx.globalAlpha = 0.3;
+		ctx.drawImage(baseAvatar, 0, 0, canvas.width, canvas.height);
+		ctx.drawImage(overlayAvatar, 0, 0, 1024, 1024);
+		res.set({ "Content-Type": "image/png" })
+		res.send(canvas.toBuffer())
+	} catch (error) {
+		res.json({ error: "Invalid image!" })
+	}
+	}
+}
