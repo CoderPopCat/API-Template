afa47a431384d9defe7f01172a7699704ec79a62
CoderPopCat <79563127+CoderPopCat@users.noreply.github.com>
2022-03-25 16:37:18 +0400

    Fixed things

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const fs = require("fs")
 
-module.exports = function(app){
-    fs.readdirSync(__dirname).forEach(function(file) {
+module.exports = function (app) {
+    fs.readdirSync(__dirname).forEach(function (file) {
         if (file == "index.js") return;
-        var name = file.substr(0, file.indexOf('.'));
+        const name = file.substr(0, file.indexOf('.'));
         const route = require('./' + name)
-                app.get(`/${route.name}`, async (req, res) => {
-                route.run(req, res)                        
-                })
+        app.get(`/${route.name}`, async (req, res) => {
+            route.run(req, res)
+        })
     });
 }
